68d74aed43c8fb80af7ea5d6f46bfb42329acbaf
Jery Ramirez <rafa85jogobonito@hotmail.com>
2020-01-21 22:14:55 -0600

    variables y comentarios

---
+++ js/02-variables.js
@@ -0,0 +1,11 @@
+// variables
+// es un contenedor de información
+
+var pais = "Guatemala";
+var continente = "America";
+var antiguedad = 2019;
+var pais_y_continente = pais+ ' '+ continente;
+pais = "salvador"
+continente = "latim"
+console.log (pais, continente, antiguedad);
+alert(pais_y_continente);
