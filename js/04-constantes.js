eddc271a8d6484038d670586b62726aa47e2297d
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-09 15:29:21 -0600

    agregando constantes

---
+++ js/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+//constantes
+//contenedor semenjante a una variable con la diferencia que su contenido nunca podra cambiar
+
+var web = "www.mibocetoensucio.com"
+const ip = "192.168.1.12"
+
+web ="www.midibujito.com"
+//ip="255.255.255.0"
+
+console.log(web)
+console.log(ip)
