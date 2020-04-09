a0c3912a215af7c5258bc12768563db0f1677190
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-09 15:18:17 -0600

    constantes

--- js/02-variables.js
+++ js/02-variables.js
@@ -6,8 +6,11 @@ var pais = "Guatemala";
 var continente = "America";
 var antiguedad = 2019;
 var pais_y_continente = pais+ ' '+ continente;
+let prueba ="hola"
+alert(prueba)
+
 pais = "salvador"
 continente = "latim"
 console.log (pais, continente, antiguedad);
 alert(pais_y_continente);
-/*modo estricto*/
+
---
+++ js/03-let-y-var.js
@@ -0,0 +1,20 @@
+'use strict'
+//pruebas con let y var
+var numero =40;
+console.log(numero);//valor 40
+
+if(true){
+    var numero = 50;
+    console.log(numero);//valor 50
+}
+
+console.log(numero);//valor
+
+var texto = "hola"
+console.log(texto);
+
+if(true){
+    let texto = "hello"
+    console.log(texto)
+}
+console.log(texto);
