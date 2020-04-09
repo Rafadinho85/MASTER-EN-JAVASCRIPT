bcf787187f6b80a406f10e3b611c430a01db007e
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-09 16:02:13 -0600

    tipos de datos y operadores

---
+++ js/05-tipos-de-datos.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//operadores
+var num1 = 7;
+var num2 = 12;
+var operacion = num1 * num2;
+alert (operacion)
+
+//tipos de datos
+var numero_entero = 45;
+var cadena_texto = "Hola 'que' tal";
+var verdadero_o_falso = false;
+
+var numero_falso = "33.4";
+
+console.log(parseFloat(numero_falso)+7);
+
+console.log(typeof numero_entero);
+console.log(typeof cadena_texto);
+console.log(typeof verdadero_o_falso);
+console.log(typeof numero_falso);
+
+
+
+
