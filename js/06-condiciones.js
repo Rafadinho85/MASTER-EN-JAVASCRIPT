9ced1b9e7f5f76c4997a8deb4d0363fbde407607
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-10 22:30:29 -0600

    ejemplo uno de if

---
+++ js/06-condiciones.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//condicional if
+//condicional nos permitira comparar dos valores y si sucede que el codigo haga algo
+
+var edad1 = 30;
+var edad2 = 12;
+
+//si pasa esto
+if(edad1 > edad2){
+    //ejecuta esto
+    console.log("La edad es mayor que edad2");
+    
+}else{
+    console.log("La edad uno es inferior");
+    
+}
