f1fe5bd3157ae5cf3cefe55ce8accefba3624f97
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-13 22:10:09 -0600

    operadores logicos

--- js/06-condiciones.js
+++ js/06-condiciones.js
@@ -15,3 +15,33 @@ if(edad1 > edad2){
     console.log("La edad uno es inferior");
     
 }
+
+//operadores logicos 
+//AND &&
+//OR ||
+//NEGACION !
+
+var year = 2018;
+if(year != 2016)
+{
+    console.log("no es 2016");
+    
+}
+
+//and 
+if(year >=2000 && year <=2020){
+    console.log("Estamos en la era actual");
+    
+}else{
+    console.log("Estamos en la era post moderna");
+    
+}
+
+//or
+if(year == 2008 || (year >= 2018 && year == 2018)){
+    console.log("El año acaba en 8");
+    
+}else{
+    console.log("año no registrado");
+    
+}
