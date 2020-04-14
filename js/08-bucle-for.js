a080b19531ef3b59e1243b8e8f529ef262d15f57
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-14 08:29:13 -0600

    bucle for

---
+++ js/08-bucle-for.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//bucle for
+//bucle es una estructura de control que se repite varias veces
+
+var numero = 100;
+
+for(var i=0; i < numero; i++){
+    console.log(i);
+    debugger
+}
