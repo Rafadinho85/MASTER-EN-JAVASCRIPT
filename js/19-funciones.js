2a4b976d9d3b091eb8deddff507c5d2ef1ac466a
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-18 22:41:18 -0600

    parametros opcionales

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -5,15 +5,27 @@
 
 //defino la funcion
 
-function calculadora(num1, num2){
+function calculadora(num1, num2, mostrar=false){
     //conjunto de instrucciones a ejectar
-    console.log("Suma: " + (num1+num2));
-    console.log("Resta: " + (num1-num2));
-    console.log("Division: " + (num1/num2));
-    console.log("Multi: " + (num1*num2));
+    if(mostrar==false){
     
+        console.log("Suma: " + (num1+num2));
+        console.log("Resta: " + (num1-num2));
+        console.log("Division: " + (num1/num2));
+        console.log("Multi: " + (num1*num2));
+        
+    }else{
+       document.write("Suma: " + (num1+num2)+"<br>");
+       document.write("Resta: " + (num1-num2)+"<br>");
+       document.write("Division: " + (num1/num2)+"<br>");
+       document.write("Multi: " + (num1*num2)+"<br>");
+    
+    }
     
 }
 //invocar o llamar a la funcion
 calculadora(8, 5);
-calculadora(888, 555);
+calculadora(888, 555,true);
+
+//parametros opcionales
+
