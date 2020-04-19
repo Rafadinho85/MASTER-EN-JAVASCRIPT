c660888ba6e18a4214251d7df393499da96005e0
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-18 22:26:52 -0600

    crear funciones

---
+++ js/19-funciones.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//funciones
+//una funcion es una agrupacion reutilizable de un conjunto de instrucciones
+
+//defino la funcion
+
+function calculadora(){
+    //conjunto de instrucciones a ejectar
+    console.log("hola soy el mejor");
+    
+}
+//invocar o llamar a la funcion
+calculadora();
