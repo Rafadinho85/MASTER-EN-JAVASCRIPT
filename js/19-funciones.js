80df7d6d780186cf35def255891a8d59f1c1f11b
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-18 22:48:39 -0600

    funciones dentro de otras funciones

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -5,27 +5,35 @@
 
 //defino la funcion
 
+function porpantalla (num1,num2){
+    console.log("Suma: " + (num1+num2));
+    console.log("Resta: " + (num1-num2));
+    console.log("Division: " + (num1/num2));
+    console.log("Multi: " + (num1*num2));
+    
+}
+function porconsola (num1, num2){
+    document.write("Suma: " + (num1+num2)+"<br>");
+    document.write("Resta: " + (num1-num2)+"<br>");
+    document.write("Division: " + (num1/num2)+"<br>");
+    document.write("Multi: " + (num1*num2)+"<br>");
+}
+
+
 function calculadora(num1, num2, mostrar=false){
     //conjunto de instrucciones a ejectar
     if(mostrar==false){
-    
-        console.log("Suma: " + (num1+num2));
-        console.log("Resta: " + (num1-num2));
-        console.log("Division: " + (num1/num2));
-        console.log("Multi: " + (num1*num2));
-        
+    porconsola(num1, num2);
+     
     }else{
-       document.write("Suma: " + (num1+num2)+"<br>");
-       document.write("Resta: " + (num1-num2)+"<br>");
-       document.write("Division: " + (num1/num2)+"<br>");
-       document.write("Multi: " + (num1*num2)+"<br>");
+     porpantalla(num1, num2);
     
     }
     
 }
 //invocar o llamar a la funcion
-calculadora(8, 5);
-calculadora(888, 555,true);
+calculadora(8, 5, true);
+calculadora(888, 555);
 
 //parametros opcionales
 
