485b8a4a7392c012d11e3c74402499e4b44eb394
Jery Ramirez <rafa85jogo@gmail.com>
2020-04-18 22:35:03 -0600

    parametros

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -5,10 +5,15 @@
 
 //defino la funcion
 
-function calculadora(){
+function calculadora(num1, num2){
     //conjunto de instrucciones a ejectar
-    console.log("hola soy el mejor");
+    console.log("Suma: " + (num1+num2));
+    console.log("Resta: " + (num1-num2));
+    console.log("Division: " + (num1/num2));
+    console.log("Multi: " + (num1*num2));
+    
     
 }
 //invocar o llamar a la funcion
-calculadora();
+calculadora(8, 5);
+calculadora(888, 555);
