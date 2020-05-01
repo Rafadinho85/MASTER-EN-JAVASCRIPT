05cc65f204fa71eac06ffff8f0f7d385c3fe51ee
libreria <rafa85jogo@gmail.com>
2020-05-01 17:47:17 -0600

    metodos de busqueda

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -4,7 +4,7 @@
 
 var numero = 444;
 var texto1 = "mi mama me mima";
-var texto2 = "kathy mi amor";
+var texto2 = "kathy mi amor kathy mi amor";
 
 //convertir un numero a un string
 var dato = numero.toString;
@@ -38,4 +38,39 @@ console.log(textoTotal);
 var textoConcat = texto1.concat(" " +texto2);
 console.log(textoConcat);
 
+//buscar texto
 
+var buscar = texto1.lastIndexOf("mama")
+console.log("buscar posicion");
+console.log(buscar);
+
+var buscar2 = texto2.search("kathy");
+console.log(buscar2);
+
+var buscar3 = texto2.match("mi");
+console.log(buscar3);
+//si queremos que nos haga una busqueda global para encontrar 
+//todas las concidencias
+var expresionregular = texto2.match(/kathy/g);
+console.log(expresionregular);
+
+//metodo substr
+
+var busqueda5 = texto2.substr(9,4);
+console.log(busqueda5);
+
+//sacar una letra en especifico 
+
+var busqueda6 = texto2.charAt(4)
+console.log(busqueda6);
+
+//buscar una palabra y devolver true o false al principio
+
+var busqueda = texto2.startsWith("kathy");
+console.log(busqueda);
+//buscar al final
+var busqueda = texto2.endsWith("amor");
+console.log(busqueda);
+//buscar la palabra y devolver un true o un false
+var busqueda = texto2.includes("Mi");
+console.log(busqueda);
