77f7d588de3c3ebbdf2a13d344d31587b9bac5df
Jery Ramirez <rafa85jogo@gmail.com>
2020-05-03 17:08:35 -0600

    arrays avanzados

--- js/25-arrays.js
+++ js/25-arrays.js
@@ -15,4 +15,22 @@ document.write(nombres)
 console.log(nombres);
 //sacar un elemento de un array
 
-console.log(lenguajes[1])
+console.log(lenguajes.length)
+/*var elemento = parseInt(prompt("Di un numero ",0));
+
+//si elemento es igual o menor que tamaño del array nombres
+if(elemento >= nombres.length){
+    
+    alert("introduce el numero correcto menor que "+nombres.length)
+}else{
+    
+    alert("El usuario seleccionado es "+nombres[elemento]);
+}
+*/
+document.write("<h1>Lenguajes</h1>");
+document.write("<ul>")
+for(var i=0; i<=lenguajes.length; i++){
+    document.write("<li>"+lenguajes[i]+"</li>");
+
+}
+document.write("</ul>")
