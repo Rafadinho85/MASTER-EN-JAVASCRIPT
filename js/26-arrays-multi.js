e743dab805898192ef6bec07f308b5b569503dc4
Jery Ramirez <rafa85jogo@gmail.com>
2020-05-03 17:52:07 -0600

    array multidimensionales

---
+++ js/26-arrays-multi.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var categorias = ['Accion', 'Terror', 'Romance'];
+var peliculas = ['Advengers','la maldicion', 'la vida es bella'];
+
+var cine = [categorias, peliculas]
+console.log(cine);
+console.log(cine[0][2]);
+console.log(cine[1][1]);
