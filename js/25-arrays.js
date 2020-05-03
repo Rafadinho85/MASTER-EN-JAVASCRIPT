f228910c27b55311254fc16cfb4f885f9d627860
Jery Ramirez <rafa85jogo@gmail.com>
2020-05-03 16:36:02 -0600

    arrays basicos

---
+++ js/25-arrays.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//arrays, arreglo, matrices
+
+//variable normal
+var nombre = "Jery"
+
+//array
+var nombres = ["Jery", "Rafael", "Helen", "Dulce", 24, 27, false];
+
+//objeto array
+var lenguajes = new Array("php", "java", "javascript");
+
+document.write(nombres)
+console.log(nombres);
+//sacar un elemento de un array
+
+console.log(lenguajes[1])
