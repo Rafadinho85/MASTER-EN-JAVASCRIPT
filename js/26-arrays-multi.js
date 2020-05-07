f6de48850f8de3cb8376154892ac9274d8b57b37
Jery Ramirez <rafa85jogo@gmail.com>
2020-05-06 19:16:52 -0600

    operaciones con array

--- js/26-arrays-multi.js
+++ js/26-arrays-multi.js
@@ -4,6 +4,37 @@ var categorias = ['Accion', 'Terror', 'Romance'];
 var peliculas = ['Advengers','la maldicion', 'la vida es bella'];
 
 var cine = [categorias, peliculas]
-console.log(cine);
-console.log(cine[0][2]);
-console.log(cine[1][1]);
+//console.log(cine);
+//console.log(cine[0][2]);
+//console.log(cine[1][1]);
+
+//operaciones con array
+
+var elemento ="";
+while(elemento != "ACABAR"){
+
+    var elemento = prompt("Agregue una pelicula");
+    peliculas.push(elemento);
+}
+//eliminar elementos con pop
+
+peliculas.pop();
+peliculas.pop();
+
+console.log(peliculas);
+
+//obtener posicion con idexOf SI APARECE MENOS -1 es porque no encontro el elemento
+/*
+var indice = peliculas.indexOf('la maldicion');
+console.log(indice);
+if(indice > -1){
+    peliculas.splice(indice);
+}
+console.log(peliculas);
+*/
+//convertir un array a texto separado por comas
+
+var pelicula_string = peliculas.join();
+console.log(pelicula_string);
+
+
